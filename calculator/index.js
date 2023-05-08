a25df43cbc75093ab69a74914e827d2aed16c77e
ankitxcoding <ankitxcoding@gmail.com>
2023-05-08 15:42:09 +0530

    calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,8 +1,10 @@
-let num1=20;
-let num2=10;
-document.getElementById("num1-el").textContent=num1;
-document.getElementById("num2-el").textContent=num2;
+let n1=prompt("Enter an integer: ");
+let n2=prompt("Enter an other integer: ");
+document.getElementById("num1-el").textContent=n1;
+document.getElementById("num2-el").textContent=n2;
 let answer=document.getElementById("ans");
+num1=parseInt(n1);
+num2=parseInt(n2);
 
 function addition() {
     answer.textContent=num1+num2;
