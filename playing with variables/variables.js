05ea000662f3e07b038588bbdfd9057d10540a22
ankitxcoding <ankitxcoding@gmail.com>
2023-05-10 16:44:00 +0530

    playing with variable

---
+++ playing with variables/variables.js
@@ -0,0 +1,44 @@
+// Example using var
+
+function exampleVar() {
+    var x = 10;
+  
+    if (true) {
+      var x = 20; // same variable as the one above
+      console.log(x); // outputs 20
+    }
+  
+    console.log(x); // outputs 20
+}
+  
+exampleVar();
+  
+// Example using let
+  
+function exampleLet() {
+    let x = 10;
+  
+    if (true) {
+      let x = 20; // different variable than the one above
+      console.log(x); // outputs 20
+    }
+  
+    console.log(x); // outputs 10
+}
+  
+exampleLet();
+  
+// Example using const
+  
+function exampleConst() {
+    const x = 10;
+  
+    if (true) {
+      const x = 20; // different variable than the one above
+      console.log(x); // outputs 20
+    }
+  
+    console.log(x); // outputs 10
+}
+  
+exampleConst();  
