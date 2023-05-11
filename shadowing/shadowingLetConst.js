25255804a7b65f3a3f84654e748659eb482c2c99
ankitxcoding <ankitxcoding@gmail.com>
2023-05-11 17:37:14 +0530

    let and const shadowing example

--- shadowing/shadowingLetConst.js
+++ shadowing/shadowingLetConst.js
@@ -1,3 +1,4 @@
+console.log("let example");
 let x=10;
 console.log(x);
 {
@@ -6,4 +7,13 @@ console.log(x);
 }
 console.log(x);
 
-// in this example let x=100 is shodowed the let x=10 only in block scope but not in the script scope because let and const are not lie on the global scope
+console.log("const example");
+let y=10;
+console.log(y);
+{
+    let y=100;
+    console.log(y);
+}
+console.log(y);
+
+// in this example let x=100 & const y=100 are shodowed the let x=10 & const y=10 only in block scope but not in the script scope because let and const are not lie on the global scope
